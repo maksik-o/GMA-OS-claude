7532d183c73b65796fef097a5bfe9203b1c68960
maksik-o <maksimgomzyakov1@gmail.com>
2026-09-21 22:21:26 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 // Настройки синхронизации. Заполняются один раз после развертывания Apps Script.
-export const API_URL = 'https://script.google.com/macros/s/AKfycbwdOYBwg4OerQt4nn_L-01zvJeHi9D7VT8V1LZ1T96XKA8vTNaauLI6DNpzee7gh_M8Vg/exec';                            // заканчивается на /exec
+export const API_URL = 'https://script.google.com/macros/s/AKfycbzbUqRnmptDWQs2zWzwQHCt26ZHzirvQ5Sbye3KNBK0UHhunJTKtefxXNgDHsLq1gCfhQ/exec';                            // заканчивается на /exec
 export const CLIENT_ID = '40124617550-80jhol56dkg6jq84l14k96d57kvrhret.apps.googleusercontent.com'; // из console.cloud.google.com
 
 // Email владельца приложения — ТОЛЬКО этому пользователю видна панель генерации ключей устройства.
